299ba427d37343c8366a632b4101884d486e8dd7
Antonio Vargas <antonio.vargas@outlook.com>
2025-11-29 15:35:16 -0800

    Better support for docket socket proxy

--- src/docker.ts
+++ src/docker.ts
@@ -1,8 +1,22 @@
 import Docker from 'dockerode';
 
-export const docker = new Docker({
-  socketPath: process.env.DOCKER_SOCKET || '/var/run/docker.sock',
-});
+const getDockerOptions = (): Docker.DockerOptions => {
+  const socket = process.env.DOCKER_SOCKET;
+
+  if (socket && socket.startsWith('tcp://')) {
+    const url = new URL(socket);
+    return {
+      host: url.hostname,
+      port: url.port,
+    };
+  }
+
+  return {
+    socketPath: socket || '/var/run/docker.sock',
+  };
+};
+
+export const docker = new Docker(getDockerOptions());
 
 export const getContainers = async () => {
   return await docker.listContainers({ all: true });
